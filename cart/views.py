e8ebfe266a1243d245ff9a2902d3631573faad19
Prakash5209 <prakashkchaudhary5209@gmail.com>
2024-11-30 15:20:03 +0545

    fixed cartitemcreateview new obj

--- cart/views.py
+++ cart/views.py
@@ -6,7 +6,7 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView,CreateAPIView,L
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from django.views import View
 from django.http import JsonResponse
-from django.db.models import Prefetch
+from django.db.models import Prefetch,Q
 
 #from store.serializers import ProductSerializer,ProductRetrieveSerializer
 from store.models import ProductModel
@@ -33,22 +33,52 @@ class CartItemCreateView(CreateAPIView):
     def create(self,request,*args,**kwargs):
         serializer = self.get_serializer(data = request.data)
         serializer.is_valid(raise_exception = True)
-
+        # necessary item
         pid = serializer.validated_data.get('product').id
         pstock = serializer.validated_data.get('product').stock
-        cart_quantity = sum([i.quantity for i in CartItem.objects.filter(product__id = pid)])
-        print('cart_quantity',cart_quantity)
-        if cart_quantity >= pstock:
-            raise ValueError("CartItemCreateView: quantity is greater than stock cannot save")
-        else:
-            try:
-                exist_or_not = CartItem.objects.get(user = self.request.user,product = serializer.validated_data['product'],color = serializer.validated_data['color'],size = serializer.validated_data['size'])
+        cart_quantity = sum([i.quantity for i in CartItem.objects.filter(Q(product__id = pid) & Q(user = self.request.user))])
+        print(request.data)
+
+        try:
+            exist_or_not = CartItem.objects.get(user = self.request.user,product = serializer.validated_data['product'],color = serializer.validated_data['color'],size = serializer.validated_data['size'])
+            if cart_quantity < pstock:
                 exist_or_not.quantity += serializer.validated_data['quantity']
                 exist_or_not.save()
                 return response.Response(serializer.data,status = status.HTTP_200_OK)
-            except CartItem.DoesNotExist:
+            else:
+                return response.Response({'status':'cart quantity is greater than pstock'},status = status.HTTP_400_BAD_REQUEST)
+        except CartItem.DoesNotExist:
+            if cart_quantity+request.data.get('quantity')<= pstock:
                 self.perform_create(serializer)
                 return response.Response(serializer.data,status = status.HTTP_201_CREATED)
+            else:
+                return response.Response({'status':'cart quantity is full'},status = status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+
+
+    #def create(self,request,*args,**kwargs):
+    #    serializer = self.get_serializer(data = request.data)
+    #    serializer.is_valid(raise_exception = True)
+
+    #    pid = serializer.validated_data.get('product').id
+    #    pstock = serializer.validated_data.get('product').stock
+    #    cart_quantity = sum([i.quantity for i in CartItem.objects.filter(product__id = pid)])
+    #    print('cart_quantity',cart_quantity)
+    #    if cart_quantity >= pstock:
+    #        print(cart_quantity)
+    #        print(pstock)
+    #        raise ValueError("CartItemCreateView: quantity is greater than stock cannot save")
+    #    else:
+    #        try:
+    #            exist_or_not = CartItem.objects.get(user = self.request.user,product = serializer.validated_data['product'],color = serializer.validated_data['color'],size = serializer.validated_data['size'])
+    #            exist_or_not.quantity += serializer.validated_data['quantity']
+    #            exist_or_not.save()
+    #            return response.Response(serializer.data,status = status.HTTP_200_OK)
+    #        except CartItem.DoesNotExist:
+    #            self.perform_create(serializer)
+    #            return response.Response(serializer.data,status = status.HTTP_201_CREATED)
 
     def perform_create(self, serializer):
             serializer.save(user = self.request.user)
